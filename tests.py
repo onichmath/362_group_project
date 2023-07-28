142813b87b7a0e15a9d2786139856e2dceaeaf7d
Matthew O <mathonich@yahoo.com>
2023-07-27 17:05:02 -0700

    Tests.py initial commit

---
+++ tests.py
@@ -0,0 +1,15 @@
+"""
+Authors: Matthew O'Malley-Nichols, TODO
+Description: TODO
+"""
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
