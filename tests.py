91f8f91d29429b0a409971a7c32ed1053c6f9e34
Devin Fahnestock <devinmfahnestock@gmail.com>
2023-08-06 13:44:00 -0400

    Added tests for helper function to convert to hex char from int

--- tests.py
+++ tests.py
@@ -3,12 +3,18 @@ Authors: Matthew O'Malley-Nichols, Pedram Jarahzedah, Devin Fahnestock
 Description: Unit tests for our 362 group project.
 """
 import unittest
+from task import conv_endian, convert_to_hex
 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
         self.assertTrue(True)
+
+    # Testing For conv_endian
+
+    def test_helper_valid(self):
+        self.assertEqual('A', convert_to_hex(10))
 
 
 if __name__ == "__main__":
