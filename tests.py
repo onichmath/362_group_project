c96290d81f31942d126695c314a3f707772b9e08
Devin Fahnestock <devinmfahnestock@gmail.com>
2023-08-06 13:53:00 -0400

    Added tests for helper function num to hex arr

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ Authors: Matthew O'Malley-Nichols, Pedram Jarahzedah, Devin Fahnestock
 Description: Unit tests for our 362 group project.
 """
 import unittest
-from task import conv_endian, convert_to_hex
+from task import conv_endian, convert_to_hex, create_hex_array
 
 
 class TestCase(unittest.TestCase):
@@ -13,8 +13,23 @@ class TestCase(unittest.TestCase):
 
     # Testing For conv_endian
 
-    def test_helper_valid(self):
+    def test_hex_ten(self):
         self.assertEqual('A', convert_to_hex(10))
+
+    def test_hex_fifteen(self):
+        self.assertEqual('F', convert_to_hex(15))
+
+    def test_hex_zero(self):
+        self.assertEqual('0', convert_to_hex(0))
+
+    def test_hexarr_1(self):
+        self.assertEqual(['F', '1'], create_hex_array(31))
+
+    def test_hexarr_2(self):
+        self.assertEqual(['1', 'F', 'A'], create_hex_array(2801))
+
+    def test_hexarr_3(self):
+        self.assertEqual(['0'], create_hex_array(0))
 
 
 if __name__ == "__main__":
